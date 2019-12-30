c0f5130ebbde778b28e2cacd1ae86f2364dd0653
Administrator <Administrator@BF-20191118ENND>
2019-12-30 14:03:19 +0800

    new Jenkins project

---
+++ src/auto/demo.java
@@ -0,0 +1,33 @@
+package auto;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class demo {
+	private Properties pro = new Properties();
+	public void ParseProperties(String propertiespath) throws java.io.IOException {
+		this.loadProperties(propertiespath);		
+	}
+	
+	private void loadProperties(String propertiespath) throws java.io.IOException {
+		try {
+			InputStream in = new FileInputStream(propertiespath);
+			pro.load(in);					
+		}catch (IOException e) {
+			e.printStackTrace();					
+		}			
+	}
+	
+	public String getTestData(String keyname) {
+		return pro.getProperty(keyname).trim();
+		
+}
+}
